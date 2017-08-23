2db2f680fe50045532654a023212e8080a4b5286
champ <champsamui8@gmail.com>
2017-08-23 11:33:10 +0700

    sss

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
         'User_ID' => '1111111111111',
         'User_Name' => 'champ',
         'User_Sex' => 'man',
-        'User_Agesssssss' => '15',
+        'User_Age' => '15',
 
 ]);
     }
