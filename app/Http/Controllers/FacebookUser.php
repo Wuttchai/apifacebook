40f06d0d286c2279f4730cb260353e321ab6aefe
champ <champsamui8@gmail.com>
2017-08-23 14:56:55 +0700

    s

--- app/Http/Controllers/FacebookUser.php
+++ app/Http/Controllers/FacebookUser.php
@@ -44,7 +44,7 @@ foreach ($fb_user['updated_time'] as $key ) {
 $time = explode(".", $dd[0]);
 $ac = users:: select("Userfacebook_ID")->get()->toarray();
 
-dd($ac);
+dd($ac[0]['Userfacebook_ID']);
 
 users::insert([
     'Userfacebook_ID' => $fb_user['id'],
