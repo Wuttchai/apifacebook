aa4b41b7a4ec8c0cf7008b118f7cd0b63d85ba9a
champ <champsamui8@gmail.com>
2017-08-23 11:41:27 +0700

    dddd

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
             DB::table('users')->insert([
-              
+                'User_ID' => '1111111111111',
+                'User_Name' => 'champ',
+                'User_Sex' => 'xxx',
+                'User_Agvsvsdvsdvsdvsdvsdvse' => 'xxx',
             ]);
         }
 }
