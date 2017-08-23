d68b6b26f6172a98c29a934002186936533ef4ed
champ <champsamui8@gmail.com>
2017-08-23 11:06:28 +0700

    dsds

--- app/user.php
+++ app/user.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class users extends Model
+class user extends Model
 {
   protected $fillable = [
       'User_ID', 'User_Name','User_Sex','User_Age'
