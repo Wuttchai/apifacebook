008d7f3fc1dca3e8cdaf1f71604dde31e352c4d2
champ <champsamui8@gmail.com>
2017-08-23 13:36:44 +0700

    sss

--- app/Http/Controllers/FacebookUser.php
+++ app/Http/Controllers/FacebookUser.php
@@ -16,25 +16,25 @@ class FacebookUser extends Controller
 
       // assuming we have a User model already set up for our database
       // and assuming facebook_id field to exist in users table in database
-dd(users::firstOrCreate(['Userfacebook_ID' => $uid]),$uid);
-      $user = users::firstOrCreate(['Userfacebook_ID' => $uid]);
+
+      //$user = users::firstOrCreate(['Userfacebook_ID' => $uid]);
 
       // get long term access token for future use
       $oAuth2Client = $fb->getOAuth2Client();
 
       // assuming access_token field to exist in users table in database
-    $user->access_token = $oAuth2Client->getLongLivedAccessToken($token)->getValue();
+    //$user->access_token = $oAuth2Client->getLongLivedAccessToken($token)->getValue();
 
 
-    $user->save();
+    //$user->save();
 
       // set default access token for all future requests to Facebook API
-      $fb->setDefaultAccessToken($user->$access_token);
+      $fb->setDefaultAccessToken($access_token);
 
       // call api to retrieve person's public_profile details
       $fields = "id,cover,name,first_name,last_name,age_range,link,gender,locale,picture,timezone,updated_time,verified";
       $fb_user = $fb->get('/me?fields='.$fields)->getGraphUser();
-
+dd($fb_user);
 
       return view('product',['fb_user'=>$fb_user]);
   }
