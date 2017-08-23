107d3515dba61c1e7d2fd7adcfa10f072dabf636
champ <champsamui8@gmail.com>
2017-08-23 11:12:59 +0700

    sss

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         DB::table('users')->delete();
-         DB::table('password_resets')->delete();
+         
     }
 }
