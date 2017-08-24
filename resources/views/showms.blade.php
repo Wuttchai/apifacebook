8b76bcd43478176168f1d1e1d0bb0feb9309e338
champ <champsamui8@gmail.com>
2017-08-24 10:40:51 +0700

    dd

---
+++ resources/views/showms.blade.php
@@ -0,0 +1,29 @@
+@extends('page.master')
+@section('content')
+<div class="row">
+  <div class="col-md-12">
+
+  </div>
+  <div class="col-md-12">
+
+  </div>
+</div>
+<div class="row">
+<p> dsdsdsdsds </p>
+    <div class="col-md-12">
+            <div class="content text-center">
+
+                          <div class="links">
+                            <button onclick="tt()" type="button" class="btn btn-primary">
+            <span> Login with Facebook</span>
+          </button>
+
+                          </div>
+                     </div>
+
+
+@endsection
+
+@section('js')
+
+@endsection
