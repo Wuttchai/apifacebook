0eb85d2737a50ee6c6376f275c4d5ffc75c90fee
champ <champsamui8@gmail.com>
2017-08-24 11:04:26 +0700

    aaa

--- app/Http/Controllers/Facebooklogout.php
+++ app/Http/Controllers/Facebooklogout.php
@@ -1,17 +1,16 @@
 <?php
 
-namespace App\Http\Controllers;
-use App\users; // you need to define the model appropriately
+namespace App\Http\Controllers; // you need to define the model appropriately
 use Facebook\Facebook;
 use Session;
-use Request;
+
 
 class FacebookUser extends Controller
 {
   public function logout() //method injection
   {
 Session::forget("User_Name");
-  
+
       return view('showms',[''=>$fb_user]);
   }
 }
