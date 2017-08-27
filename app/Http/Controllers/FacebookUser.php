6a498bb064881dab91418583a85e7360b144f4f2
champsamui8@gmail.com <champsamui8@gmail.com>
2017-08-27 19:40:06 +0700

    ggg

--- app/Http/Controllers/FacebookUser.php
+++ app/Http/Controllers/FacebookUser.php
@@ -48,10 +48,13 @@ foreach ($fb_user['likes'] as $key ) {
   echo "รหัสเพจ : ".$key['id'].'<br>';
 echo "ชื่อเพจ : ".$key['name'].'<br>';
 echo "วันที่สร้างเพจ : ".$key["created_time"]->format('Y-m-d H:i').'<br>';
-$data_get_link = file_get_contents("https://graph.facebook.com/v2.8/".$key['id']."?fields=id%2Cname%2Clink%2Ccategory%2Cproducts%2Cfan_count&access_token=".$access_token);
+$data_get_link = file_get_contents("https://graph.facebook.com/v2.8/".$key['id']."?fields=id%2Cname%2Clink%2Ccategory%2products%2Cfan_count&access_token=".$access_token);
 $data_get_link = json_decode($data_get_link);
 echo "ลิงค์ : ".$data_get_link->link .'<br>';
 echo "หมวดหมู่ : ".$data_get_link->products .'<br>';
+if (condition) {
+  # code...
+}
 echo "หมวดหมู่ : ".$data_get_link->category .'<br>';
 echo "ยอดคนกดไลค์ : ".$data_get_link->fan_count .'<br>';
   $num++;
