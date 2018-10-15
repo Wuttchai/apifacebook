f5409833da4fc7ec0a4ea9909200d423a6d523b8
Wuttchai <champsamui8@gmail.com>
2018-10-15 13:32:20 +0700

    t

--- app/Http/Controllers/FacebookUser.php
+++ app/Http/Controllers/FacebookUser.php
@@ -48,7 +48,7 @@ foreach ($fb_user['likes'] as $key ) {
   echo "รหัสเพจ : ".$key['id'].'<br>';
 echo "ชื่อเพจ : ".$key['name'].'<br>';
 echo "วันที่สร้างเพจ : ".$key["created_time"]->format('Y-m-d H:i').'<br>';
-//$data_get_link = file_get_contents("https://graph.facebook.com/v2.8/".$key['id']."?fields=id%2Cname%2Clink%2Ccategory%2products%2Cfan_count&access_token=".$access_token);
+///$data_get_link = file_get_contents("https://graph.facebook.com/v2.8/".$key['id']."?fields=id%2Cname%2Clink%2Ccategory%2products%2Cfan_count&access_token=".$access_token);
 $data_get_link = file_get_contents("https://graph.facebook.com/ ".$key['id']."?access_token=".$access_token."&fields=name,likes");
 
 https://graph.facebook.com/FANPAGE_ID?access_token=ACCESS_TOKEN&fields=name,likes
